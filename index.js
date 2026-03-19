f1d12ef5bcc8eec0df594f30cd8757b5931f723d
SUJITH-L <sujithlogu22@gmail.com>
2026-03-19 21:27:24 +0530

    Enhance input handling with local storage and delete option
    Added functionality to save and retrieve URLs from local storage. Implemented double-click event to clear the list.

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 let inpEl=document.getElementById("input-El")
 let arr=[]
 let btnEl=document.getElementById("inp-btn")
+let delEl=document.getElementById("del-btn")
 let list=document.getElementById("unorder")
+let tab=document.getElementById("tab-btn")
+let localstorage1=JSON.parse(localStorage.getItem("arr"))
+console.log(localstorage1)
+if (localstorage1){
+    arr=localstorage1
+    renderit()
+}
+
+delEl.addEventListener("dblclick",function(){
+    localStorage.clear()
+    arr=[]
+    renderit()
+})
+tab.addEventListener("click",function(){
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    console.log(tabs[0].url);
+    arr.push(tabs[0].url)
+    localStorage.setItem("arr",JSON.stringify(arr))
+    renderit()
+})
+})
 
 btnEl.addEventListener("click",function(){
     arr.push(inpEl.value)
     inpEl.value=""
+    localStorage.setItem("arr",JSON.stringify(arr))
     renderit()
+    console.log(localStorage.getItem("arr"))
 
 })
 function renderit(){
